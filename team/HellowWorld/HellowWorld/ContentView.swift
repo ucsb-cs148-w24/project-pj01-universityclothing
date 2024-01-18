80f9f85889ab7835b0982692c7f903fbb43c4f0e
Zhen Bi <junebi@JunedeMacBook-Pro.local>
2024-01-17 22:12:04 -0800

    zhenbi hello world

---
+++ team/HellowWorld/HellowWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HellowWorld
+//
+//  Created by June Bi on 1/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ team/HellowWorld/HellowWorld/HellowWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HellowWorldApp.swift
+//  HellowWorld
+//
+//  Created by June Bi on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct HellowWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
