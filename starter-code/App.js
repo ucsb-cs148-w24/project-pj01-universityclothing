0ee774355d6dde83b1a5a41e3cb8ce2c4959b3c1
pingyujc <pingyujc@gmail.com>
2024-02-05 18:33:30 -0800

    can now add to firebase db using template in PostDBTest.js

--- starter-code/App.js
+++ starter-code/App.js
@@ -6,13 +6,15 @@ import ChatScreen from "./src/screen/ChatScreen";
 import LoginScreen from "./src/screen/LoginScreen";
 import Profile from "./src/screen/Profile";
 import SearchScreen from "./src/screen/SearchScreen";
+import PostDBTest from "./src/screen/PostDBTest";
 
 export default function App() {
     return (
         <View style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
             {/* <StatusBar style="auto" /> */}
-            <ReadDBTest />
+            {/* <ReadDBTest /> */}
+            <PostDBTest />
         </View>
     );
 }
--- starter-code/src/screen/PostDBTest.js
+++ starter-code/src/screen/PostDBTest.js
@@ -1,36 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { firestore } from "../../firebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, doc, setDoc } from "firebase/firestore";
 
 export default function PostDBTest() {
     // TODO change this to post instead of read
-    
-    const [users, setUsers] = useState([]);
 
-    async function getUsers() {
-        console.log("calling getUsers");
+    const [name, setName] = useState("");
 
+    // this function will take email as id (primary key), and data and write it to user database
+    async function addUser(id, data) {
+        console.log("Adding user with id:", id);
+
+        // Reference to the users collection
         const usersCol = collection(firestore, "users");
-        const userSnapshot = await getDocs(usersCol);
-        const userList = userSnapshot.docs.map((doc) => doc.data());
 
-        console.log(userList);
+        // console.log(usersCol);
 
-        setUsers(userList);
+        // Add a new document with a generated id and log the result
 
-        console.log(users);
+        const docRef = await setDoc(doc(firestore, "users", id), data);
     }
 
     useEffect(() => {
-        getUsers();
+        // modify the data when needed
+        const data = {
+            fname: "testing",
+            lname: "test2",
+            school: "ucsb",
+        };
+        const id = "test@gmail.com";
+        addUser(id, data);
     }, []);
 
+    // Placeholder content, adjust as needed
     return (
         <View>
-            {
-                users.map((user, i) => <Text key={i}>{JSON.stringify(user)}</Text>)
-            }
+            <Text>User added to Firestore!</Text>
         </View>
     );
 }
