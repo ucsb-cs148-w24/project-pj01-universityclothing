a7c0da98d7a08231924e34d27b6ef03ecc2a90ec
Zhen Bi <junebi@JunedeMacBook-Pro.local>
2024-02-08 16:44:45 -0800

    2.0 search screen

--- starter-code/src/theme/theme.ts
+++ starter-code/src/theme/theme.ts
@@ -1,14 +1,14 @@
 interface Spacing {
-    space_2: 2;
-    space_4: 4;
-    space_6: 6;
-    space_8: 8;
-    space_10: 10;
-    space_12: 12;
-    space_14: 14;
-    space_16: 16;
-    space_18: 18;
-    space_20: 20;
+    space_2: number;
+    space_4: number;
+    space_6: number;
+    space_8: number;
+    space_10: number;
+    space_12: number;
+    space_14: number;
+    space_16: number;
+    space_18: number;
+    space_20: number;
   }
   
   export const SPACING: Spacing = {
@@ -29,6 +29,9 @@ interface Spacing {
       lightBlue: string;
       yellow: string;
       lightYellow: string;
+      lightGrey:string;
+      black: string;
+      white:string;
   }
 
   
@@ -37,6 +40,9 @@ interface Spacing {
     lightBlue: '#0174BE',
     yellow: '#FFC436',
     lightYellow: '#FFF0CE',
+    lightGrey: '#E3E3E3',
+    black: '#000000',
+    white: '#FFFFFF',
   };
   
   interface FontFamily {
