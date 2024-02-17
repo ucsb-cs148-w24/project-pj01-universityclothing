f237c76cc5898d4e5ffa5b0f940898e297bba44d
xinyao-song <xinyaosong@ucsb.edu>
2024-02-16 21:09:16 -0800

    edited package.json for jest

--- starter-code/tests/HomeCompile.test.js
+++ starter-code/tests/HomeCompile.test.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import HomeScreen from '../HomeScreen'; // Adjust the import path as necessary
+import HomeScreen from '../src/screen/HomeScreen';
 import { ItemsContext } from '../components/ItemsContext'; // If you use Context
 
-test('HomeScreen renders correctly', () => {
-  // Mock any necessary values
-  const mockItems = [
-    // ... your mock items here
-  ];
-
-  const tree = renderer.create(
-    <ItemsContext.Provider value={{ items: mockItems }}>
-      <HomeScreen />
-    </ItemsContext.Provider>
-  ).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
+test('HomeScreen renders without crashing', () => {
+    const { unmount } = render(<HomeScreen />);
+    unmount();
+  });
