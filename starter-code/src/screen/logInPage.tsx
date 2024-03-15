30f0ca88e374a87dc61d13b5ee381ef509a33583
xinyao-song <xinyaosong@ucsb.edu>
2024-03-14 22:20:02 -0700

    minor change

--- starter-code/src/screen/logInPage.tsx
+++ starter-code/src/screen/logInPage.tsx
@@ -221,7 +221,7 @@ const styles = StyleSheet.create({
         borderRadius: 20, // Border-radius of 20px
         padding: 10, // Add padding for better visual appearance
         alignSelf: "center", // Center content horizontally
-        marginTop: "100%",
+        marginTop: "160%",
         width: "70%",
     },
     googleSignInButtonText: {
