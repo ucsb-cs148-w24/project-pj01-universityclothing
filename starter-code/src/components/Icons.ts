c7b4a1d0a384a44127fcc78f6dd3f40843fc19ee
xinyao-song <xinyaosong@ucsb.edu>
2024-03-14 20:04:37 -0700

    modified UI for navigation bar and post screen

--- starter-code/src/components/Icons.ts
+++
@@ -1,5 +0,0 @@
-{/*install by running:
-yarn add react-native-vector-icons */}
-import {createIconSetFromIcoMoon} from 'react-native-vector-icons';
-import icoMoonConfig from '../selection.json';
-export default createIconSetFromIcoMoon(icoMoonConfig);
